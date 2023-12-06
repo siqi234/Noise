6b984c4cd099cb94246ae4043b24e8981fbb1f12
Vickie1234 <siqilu18@outlook.com>
2023-12-05 23:02:37 -0600

    minor changes

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from dataloader import AudioDataset  # Assuming you have a dataset class defined
 from torch.utils.data import DataLoader
 from utils import save_model, load_model  # Assuming these functions are defined in utils.py
 import torch.nn.functional as F
+from tqdm import tqdm
 
 # Parameters
 learning_rate = 0.001
@@ -26,29 +27,32 @@ model = AudioDenoisingCNN().to(device)
 criterion = nn.MSELoss()  # Mean Squared Error is common for regression tasks
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-# Training loop
+# Training loop with tqdm progress bar
 for epoch in range(num_epochs):
     model.train()
-    for i, (noisy, clear) in enumerate(train_loader):
-        # Add a channel dimension to the input
-        noisy, clear = noisy.unsqueeze(1).to(device), clear.unsqueeze(1).to(device)
+    epoch_loss = 0
+    with tqdm(train_loader, unit="batch") as tepoch:
+        for i, (noisy, clear) in enumerate(tepoch):
+            tepoch.set_description(f"Epoch {epoch+1}")
 
-        # Forward pass
-        outputs = model(noisy)
+            noisy, clear = noisy.unsqueeze(1).to(device), clear.unsqueeze(1).to(device)
 
-        # Resize output to match target tensor size
-        outputs_resized = F.interpolate(outputs, size=(1025, 130), mode='bilinear', align_corners=False)
+            # Forward pass
+            outputs = model(noisy)
 
-        # Calculate loss
-        loss = criterion(outputs_resized, clear)
+            # Resize output to match target tensor size
+            outputs_resized = F.interpolate(outputs, size=(1025, 130), mode='bilinear', align_corners=False)
 
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            # Calculate loss
+            loss = criterion(outputs_resized, clear)
 
-        if (i + 1) % 10 == 0:
-            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader)}], Loss: {loss.item():.4f}')
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            epoch_loss += loss.item()
+            tepoch.set_postfix(loss=epoch_loss / (i + 1))
 
 # Save the model after training
 save_model(model, 'audio_denoising_model.pth')
